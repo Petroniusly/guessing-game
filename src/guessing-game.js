f6b74cd0cffd18d95d123aa5d73d0c1ad096b793
Petroniusly <blinkovpag@gmail.com>
2017-09-29 16:33:47 +0300

    Task guessing-game complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.answer;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.max == this.min) {
+            return this.answer = this.min;
+        } else if (this.max - this.min == 1) {
+            return this.answer = this.min + 1;
+        } else {
+            return this.answer = Math.ceil((this.max - this.min) / 2 + this.min);
+        }
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
